7a13ae5bbc80fabe030f5ff5ca150bc48baf5dbc
Thomas <thomas.haack.christensen@gmail.com>
2020-03-18 14:17:01 +0100

    Added some function such as Button_Click_1.

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -29,5 +29,56 @@ namespace Calculator
         {
 
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            tb.Text += b.Content.ToString();
+        }
+
+        private void Result_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                result();   
+            }
+            catch (Exception exc)
+            {
+
+                tb.Text = "Error!";
+            }
+        }
+
+        private void result()
+        {
+            string op;
+            int iOp = 0;
+            if (tb.Text.Contains("+"))
+            {
+                iOp = tb.Text.IndexOf("+");
+            }
+            else if (tb.Text.Contains("-"))
+            {
+                iOp = tb.Text.IndexOf("-");
+            }
+            else if (tb.Text.Contains("*"))
+            {
+                iOp = tb.Text.IndexOf("*");
+            }
+            else if (tb.Text.Contains("/"))
+            {
+                iOp = tb.Text.IndexOf("/");
+            }
+            else
+            {
+                //Error
+            }
+
+            op = tb.Text.Substring(iOp, 1);
+            double op1 = Convert.ToDouble(tb.Text.Substring(0, iOp));
+            double op2 = Convert.ToDouble(tb.Text.Substring(iOp + 1, tb.Text.Length - iOp - 1));
+
+
+        }
     }
 }
