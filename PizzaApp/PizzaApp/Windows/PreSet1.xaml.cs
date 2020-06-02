f711e768cfb3eba4dbf0c26c9fcbe9e7fb137374
Thomas <Thomas.haack.christensen@gmail.com>
2020-06-02 10:56:44 +0200

    New Repo changes

---
+++ PizzaApp/PizzaApp/Windows/PreSet1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PizzaApp.Properties;
+
+namespace PizzaApp.Windows
+{
+    /// <summary>
+    /// Interaction logic for PreSet1.xaml
+    /// </summary>
+    public partial class PreSet1 : Window
+    {
+        public PreSet1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            MessageBox.Show("Du har Bestilt en Pizza Nummer 1 til 40,- kr ");
+        }
+    }
+}
---
+++ PizzaApp/PizzaApp/Windows/PreSet2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PizzaApp.Windows
+{
+    /// <summary>
+    /// Interaction logic for PreSet2.xaml
+    /// </summary>
+    public partial class PreSet2 : Window
+    {
+        public PreSet2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            MessageBox.Show("Du har bestilt en Pizza nummer 2 til 58,- kr");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
